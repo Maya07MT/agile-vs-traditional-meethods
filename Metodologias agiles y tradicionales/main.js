082e5ee8e40c20b545ed4b03688e98177e61e767
Maya07MT <113948832+Maya07MT@users.noreply.github.com>
2023-03-02 09:41:13 -0600

    Add files via upload

---
+++ Metodologias agiles y tradicionales/main.js
@@ -0,0 +1,23 @@
+function seleccionar(link) {
+  var opciones = document.querySelectorAll('#links  a');
+  opciones[0].className = "";
+  opciones[1].className = "";
+  opciones[2].className = "";
+  opciones[3].className = "";
+  opciones[4].className = "";
+  link.className = "seleccionado";
+
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('La Capital del Sol')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
+}
